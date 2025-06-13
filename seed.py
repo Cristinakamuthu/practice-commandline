8eae301440d36b972b64aa6e0a3d63a8c8fb1eae
Cristina <kamuthucristina@gmail.com>
2025-06-13 18:18:54 +0300

    fixed the arguments

--- seed.py
+++ seed.py
@@ -7,10 +7,10 @@ from models import Director
 Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
 
-actor1 = Actor(name='Cristina Wanjiku', age=24)
-actor2 = Actor(name='Michael Mramba', age=30)
+actor1 = Actor(name='Cristina Nyawira', age=24)
+actor2 = Actor(name='Francisca Kamuthu', age=30)
 
-director1 = Director(name='Matthew', age=45)
+director1 = Director(name='Marion Wangare', age=45)
 
 movie1 = Movie(title='Rise of the Devs', year=2025, director=director1, actor=actor1)
 movie2 = Movie(title='Codebound', year=2025, director=director1, actor=actor2)
